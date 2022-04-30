eae02a4f255bd92bf6584bd0ae102e136213661f
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-30 17:52:41 -0500

    user seeds added

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,6 +1,19 @@
 const { User } = require("../models");
 
-const userdata = [];
+const userdata = [
+    {
+        "username": "violeteyes",
+        "password": "password123"
+    },
+    {
+        "username": "aprilshowers",
+        "password": "123password"
+    },
+    {
+        "username": "sleep4theweak",
+        "password": "pass123word"
+    }
+];
 
 const seedUsers = () => User.bulkCreate(userdata);
 
