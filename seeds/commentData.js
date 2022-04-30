334516a80400934805b1f2f1efcadadf5e5da724
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-30 18:05:38 -0500

    forgot dates, ready to test seeding

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -5,19 +5,22 @@ const commentdata = [
         title: "TEST 1",
         content: "Test content",
         post_id: 3,
-        user_id: 1
+        user_id: 1,
+        date_created: "April 20, 2021 09:00:00"
     },
     {
         title: "TEST 2",
         content: "More test content",
         post_id: 1,
-        user_id: 2
+        user_id: 2,
+        date_created: "April 21, 2021 17:00:00"
     },
     {
         title: "TEST 3",
         content: "Still more test content.",
         post_id: 2,
-        user_id: 3
+        user_id: 3,
+        date_created: "April 22, 2021 12:00:00"
     },
 ];
 
--- seeds/postData.js
+++ seeds/postData.js
@@ -1,6 +1,25 @@
 const { Post } = require("../models");
 
-const postdata = [];
+const postdata = [
+    {
+        title: "TEST 1",
+        content: "Test content",
+        user_id: 2,
+        date_created: "April 20, 2021 07:00:00"
+    },
+    {
+        title: "TEST 2",
+        content: "More test content",
+        user_id: 3,
+        date_created: "April 21, 2021 07:00:00"
+    },
+    {
+        title: "TEST 3",
+        content: "Still more test content.",
+        user_id: 1,
+        date_created: "April 22, 2021 07:00:00"
+    },
+];
 
 const seedPosts = () => Post.bulkCreate(postdata);
 
