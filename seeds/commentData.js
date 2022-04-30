bd2bf8aaa47d1e7bf8b9f675580de3e305b49e5d
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-04-30 17:56:03 -0500

    comment seeds

--- seeds/commentData.js
+++ seeds/commentData.js
@@ -1,6 +1,25 @@
 const { Comment } = require("../models");
 
-const commentdata = [];
+const commentdata = [
+    {
+        title: "TEST 1",
+        content: "Test content",
+        post_id: 3,
+        user_id: 1
+    },
+    {
+        title: "TEST 2",
+        content: "More test content",
+        post_id: 1,
+        user_id: 2
+    },
+    {
+        title: "TEST 3",
+        content: "Still more test content.",
+        post_id: 2,
+        user_id: 3
+    },
+];
 
 const seedComments = () => Comment.bulkCreate(commentdata);
 
