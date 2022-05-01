23da109da2d8c99fee260ff5e3567f88c3c36bad
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-05-01 10:48:13 -0500

    working on routing, endpoint reached, no page to render

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,19 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
 // post user
+
+router.post("/", async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 // get user
 
 // delete user
+
+module.exports = router;
---
+++ controllers/pageRoutes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+    res.render("homepage");
+});
+
+router.get("/login", (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect("/");
+        return;
+    }
+
+    res.render("login");
+});
+
+module.exports = router;
