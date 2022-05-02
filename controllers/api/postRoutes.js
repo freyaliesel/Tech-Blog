9a91dc98d5da21ccd83736661e374116d0630c52
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-05-02 13:48:29 -0500

    grabbed some logic from lessons, needs adjusting for blog use

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,5 +1,43 @@
+const router = require("express").Router();
+const { Post } = require("../../models");
+
+// get all posts
+router.get("/", async (req, res) => {
+    try {
+        const postData = await Post.findAll();
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render("homepage", {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// get single post
+router.get("/posts/:id", async (req, res) => {
+    if (!req.session.loggedIn) {
+        res.redirect("/login");
+    } else {
+        try {
+            const postData = await Post.findByPk(req.params.id);
+
+            const post = postData.get({ plain: true });
+
+            res.render("post", {
+                post,
+                loggedIn: req.session.loggedIn,
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    }
+});
+
 // post post
-
-// get post
-
 // delete post
