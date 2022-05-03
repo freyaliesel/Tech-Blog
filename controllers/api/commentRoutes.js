66347f512993ca8f8d66b9d6289864dbad6c10c4
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-05-03 16:48:01 -0500

    adjustments to routing

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,5 +1,19 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const { withAuth } = require("../../utils/auth");
+
 // post comment
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
 
-// get comment
+        res.status(201).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
-// delete comment
+module.exports = router;
--- controllers/pageRoutes.js
+++ controllers/pageRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const withAuth = require("../utils/auth");
+const { User, Post } = require("../models");
 
 router.get("/", async (req, res) => {
     res.render("homepage");
@@ -12,6 +13,27 @@ router.get("/login", (req, res) => {
     }
 
     res.render("login");
+});
+
+router.get("/dashboard/:id", withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.params.id);
+        const user = userData.get({ plain: true });
+
+        const postData = await Post.findAll({
+            where: { user_id: req.params.id },
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render("dashboard", {
+            user,
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
