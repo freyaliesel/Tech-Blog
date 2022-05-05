976fc45f4b3edd420ab8329c9cfd0585af4a25c8
freyaliesel <80221739+freyaliesel@users.noreply.github.com>
2022-05-05 12:34:39 -0500

    adjusted user model, seeds

--- models/user.js
+++ models/user.js
@@ -12,9 +12,17 @@ User.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        user_name: {
+        username: {
             type: DataTypes.STRING,
             allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
         },
         password: {
             type: DataTypes.STRING,
--- seeds/userData.js
+++ seeds/userData.js
@@ -1,19 +1,29 @@
 const { User } = require("../models");
+const bcrypt = require("bcrypt");
 
 const userdata = [
     {
-        "user_name": "violeteyes",
-        "password": "password123"
+        username: "violeteyes",
+        email: "violeteyes@email.com",
+        password: "password123",
     },
     {
-        "user_name": "aprilshowers",
-        "password": "123password"
+        username: "aprilshowers",
+        email: "aprilshowers@email.com",
+        password: "123password",
     },
     {
-        "user_name": "sleep4theweak",
-        "password": "pass123word"
-    }
+        username: "sleep4theweak",
+        email: "sleep4theweak@email.com",
+        password: "pass123word",
+    },
 ];
+
+User.beforeBulkCreate(async (users) => {
+    for (const user of users) {
+        user.password = await bcrypt.hash(user.password, 10);
+        }
+});
 
 const seedUsers = () => User.bulkCreate(userdata);
 
